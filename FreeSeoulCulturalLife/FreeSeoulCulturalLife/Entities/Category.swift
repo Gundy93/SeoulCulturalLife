9cd527685e0a398e7e7981a47be6ec1ed36486cd
Gundy <korea1880@naver.com>
2023-09-25 10:27:41 +0900

    feat: 행사의 종류를 정의하는 Category 열거형 추가

---
+++ FreeSeoulCulturalLife/FreeSeoulCulturalLife/Entities/Category.swift
@@ -0,0 +1,23 @@
+//
+//  Category.swift
+//  FreeSeoulCulturalLife
+//
+//  Created by Gundy on 2023/09/25.
+//
+
+enum Category {
+    
+    case culture
+    case art
+    case exhibition
+    case traditionalMusic
+    case musical
+    case classic
+    case concert
+    case recital
+    case dancing
+    case theater
+    case movie
+    case festival
+    case etc
+}
