16cd7b72db6f9baf971852285effbc368520c0ae
Gundy <korea1880@naver.com>
2023-09-25 15:29:51 +0900

    feat: 행사와 관련된 기본 정보를 모델링하는 Event 구조체 정의

---
+++ FreeSeoulCulturalLife/FreeSeoulCulturalLife/Entities/Event.swift
@@ -0,0 +1,18 @@
+//
+//  Event.swift
+//  FreeSeoulCulturalLife
+//
+//  Created by Gundy on 2023/09/25.
+//
+
+import Foundation
+
+struct Event {
+    
+    let title: String
+    let category: Category
+    let gu: Gu
+    let imageLink: URL
+    let startDate: Date
+    let endDate: Date
+}
