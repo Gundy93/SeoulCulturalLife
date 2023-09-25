6198c64ba36b241b3e4cc137214273d5a0caa2cc
Gundy <korea1880@naver.com>
2023-09-25 15:23:23 +0900

    feat: Category 열거형의 원시값 추가

--- FreeSeoulCulturalLife/FreeSeoulCulturalLife/Entities/Category.swift
+++ FreeSeoulCulturalLife/FreeSeoulCulturalLife/Entities/Category.swift
@@ -5,19 +5,18 @@
 //  Created by Gundy on 2023/09/25.
 //
 
-enum Category {
+enum Category: String {
     
-    case culture
-    case art
-    case exhibition
-    case traditionalMusic
-    case musical
-    case classic
-    case concert
-    case recital
-    case dancing
-    case theater
-    case movie
-    case festival
-    case etc
+    case culture = "문화교양/강좌"
+    case exhibition = "전시/미술"
+    case traditionalMusic = "국악"
+    case musical = "뮤지컬/오페라"
+    case classic = "클래식"
+    case concert = "콘서트"
+    case recital = "독주/독창회"
+    case dancing = "무용"
+    case theater = "연극"
+    case movie = "영화"
+    case festival = "축제"
+    case etc = "기타"
 }
