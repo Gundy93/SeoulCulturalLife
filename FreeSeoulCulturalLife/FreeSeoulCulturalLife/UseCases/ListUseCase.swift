ab368d649c21feeecfb6cea8eec86d4a69f58e78
Gundy <korea1880@naver.com>
2023-09-25 23:04:07 +0900

    feat: List 화면의 UseCase인 ListUseCase 정의

---
+++ FreeSeoulCulturalLife/FreeSeoulCulturalLife/UseCases/ListUseCase.swift
@@ -0,0 +1,30 @@
+//
+//  ListUseCase.swift
+//  FreeSeoulCulturalLife
+//
+//  Created by Gundy on 2023/09/25.
+//
+
+import Foundation
+
+struct ListUseCase: UseCase {
+    
+    var delegate: UseCaseDelegate?
+    var container: [Event] = [] {
+        didSet {
+            delegate?.useCaseDidUpdate(events: container, current)
+        }
+    }
+    var informations: [Event: Information] = [:]
+    var current: [Event] = []
+    
+    mutating func setContainer(_ container: [Event]) {
+        current = container
+        self.container = container
+    }
+    
+    mutating func appendEvents(_ events: [Event]) {
+        current = events
+        container += events
+    }
+}
