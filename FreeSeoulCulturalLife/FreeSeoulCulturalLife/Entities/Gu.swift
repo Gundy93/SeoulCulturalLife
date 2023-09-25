812bf0bb703e8e8b0d94f5414d2288441df4c099
Gundy <korea1880@naver.com>
2023-09-25 10:50:46 +0900

    feat: 서울시 자치구를 정의하는 Gu 열거형 추가

---
+++ FreeSeoulCulturalLife/FreeSeoulCulturalLife/Entities/Gu.swift
@@ -0,0 +1,35 @@
+//
+//  Gu.swift
+//  FreeSeoulCulturalLife
+//
+//  Created by Gundy on 2023/09/25.
+//
+
+enum Gu: String {
+    
+    case jongno = "종로구"
+    case jung = "중구"
+    case yongsan = "용산구"
+    case seongdong = "성동구"
+    case gwangjin = "광진구"
+    case dongdaemun = "동대문구"
+    case jungnang = "중랑구"
+    case seongbuk = "성북구"
+    case gangbuk = "강북구"
+    case dobong = "도봉구"
+    case nowon = "노원구"
+    case eunpyeong = "은평구"
+    case seodaemun = "서대문구"
+    case mapo = "마포구"
+    case yangcheon = "양천구"
+    case gangseo = "강서구"
+    case guro = "구로구"
+    case geumcheon = "금천구"
+    case yeongdeungpo = "영등포구"
+    case dongjak = "동작구"
+    case gwanak = "관악구"
+    case seocho = "서초구"
+    case gangnam = "강남구"
+    case songpa = "송파구"
+    case gangdong = "강동구"
+}
