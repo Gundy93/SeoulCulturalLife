ce36e42269b5991267d52babd86998496819f5d7
Gundy <korea1880@naver.com>
2023-09-25 18:09:22 +0900

    feat: 행사의 상세 정보를 모델링하는 Information 구조체 생성

---
+++ FreeSeoulCulturalLife/FreeSeoulCulturalLife/Entities/Information.swift
@@ -0,0 +1,17 @@
+//
+//  Information.swift
+//  FreeSeoulCulturalLife
+//
+//  Created by Gundy on 2023/09/25.
+//
+
+struct Information {
+    
+    let place: String
+    let homePage: String
+    let portal: String
+    let player: String?
+    let useTarget: String?
+    let program: String?
+    let description: String?
+}
