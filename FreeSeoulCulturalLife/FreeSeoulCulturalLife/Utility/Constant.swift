5fc9fe9dd168be4ac13a1f1940e218e49813012d
Gundy <korea1880@naver.com>
2023-09-26 16:57:12 +0900

    feat: name space로 사용하기 위한 Constant 열거형 정의

---
+++ FreeSeoulCulturalLife/FreeSeoulCulturalLife/Utility/Constant.swift
@@ -0,0 +1,13 @@
+//
+//  Constant.swift
+//  FreeSeoulCulturalLife
+//
+//  Created by Gundy on 2023/09/26.
+//
+
+import Foundation
+
+enum Constant {
+    
+    static let defaultPostName: Notification.Name = Notification.Name("default")
+}
