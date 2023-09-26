85d73ec220d76e4db338266f0ad3bcb97d8c9ab1
Gundy <korea1880@naver.com>
2023-09-27 00:02:09 +0900

    feat: List 화면의 기본 UI를 모델링한 ListViewController 정의

---
+++ FreeSeoulCulturalLife/FreeSeoulCulturalLife/FrameworksAndDrivers/ListViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ListViewController.swift
+//  FreeSeoulCulturalLife
+//
+//  Created by Gundy on 2023/09/22.
+//
+
+import UIKit
+
+final class ListViewController: UIViewController {
+    
+    private var viewModel: ViewModel
+    private var listTableView: UITableView = {
+        let tableView = UITableView()
+        
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return tableView
+    }()
+    
+    init(viewModel: ViewModel) {
+        self.viewModel = viewModel
+        
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureNavigationBar()
+        configureViewHierarchy()
+    }
+    
+    private func configureNavigationBar() {
+        navigationItem.title = "목록"
+    }
+    
+    private func configureViewHierarchy() {
+        let safeArea = view.safeAreaLayoutGuide
+        
+        view.addSubview(listTableView)
+        NSLayoutConstraint.activate([
+            listTableView.topAnchor.constraint(equalTo: safeArea.topAnchor),
+            listTableView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor),
+            listTableView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
+            listTableView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor)
+        ])
+    }
+}
+
--- FreeSeoulCulturalLife/FreeSeoulCulturalLife/InterfaceAdapters/ViewModel/ViewModel.swift
+++ FreeSeoulCulturalLife/FreeSeoulCulturalLife/InterfaceAdapters/ViewModel/ViewModel.swift
@@ -14,6 +14,7 @@ class ViewModel: UseCaseDelegate {
     
     init(useCase: UseCase) {
         self.useCase = useCase
+        useCase.delegate = self
     }
     
     func setFilter(gu: Gu?, date: Date?) {
