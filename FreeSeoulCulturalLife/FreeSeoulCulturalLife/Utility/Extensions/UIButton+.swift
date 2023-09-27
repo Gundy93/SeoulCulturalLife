5ef443f0fdb6627f6e049366d931b751ddc6ac1a
Gundy <korea1880@naver.com>
2023-09-27 18:22:43 +0900

    feat: extension을 통해 UIButton에 편의 이니셜라이저 추가

---
+++ FreeSeoulCulturalLife/FreeSeoulCulturalLife/Utility/Extensions/UIButton+.swift
@@ -0,0 +1,17 @@
+//
+//  UIButton+.swift
+//  FreeSeoulCulturalLife
+//
+//  Created by Gundy on 2023/09/27.
+//
+
+import UIKit
+
+extension UIButton {
+    
+    convenience init(primaryAction: UIAction?,
+                     image: UIImage?) {
+        self.init(primaryAction: primaryAction)
+        setImage(image, for: .normal)
+    }
+}
