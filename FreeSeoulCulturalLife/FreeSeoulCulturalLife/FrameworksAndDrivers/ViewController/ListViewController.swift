9ca57b4b162b81c66dfd4318e5892792afef32bd
Gundy <korea1880@naver.com>
2023-09-27 14:32:15 +0900

    feat: listTableView와 listDataSource의 초기설정을 하는 메서드 추가

--- FreeSeoulCulturalLife/FreeSeoulCulturalLife/FrameworksAndDrivers/ViewController/ListViewController.swift
+++ FreeSeoulCulturalLife/FreeSeoulCulturalLife/FrameworksAndDrivers/ViewController/ListViewController.swift
@@ -12,10 +12,12 @@ final class ListViewController: UIViewController {
     typealias ListDataSource = UITableViewDiffableDataSource<Int, Event>
     typealias ListSnapShot = NSDiffableDataSourceSnapshot<Int, Event>
     
-    private var viewModel: ViewModel
-    private var listTableView: UITableView = {
+    private let viewModel: ViewModel
+    private let listTableView: UITableView = {
         let tableView = UITableView()
         
+        tableView.register(ListCell.self,
+                           forCellReuseIdentifier: Constant.ListCellIdentifier)
         tableView.translatesAutoresizingMaskIntoConstraints = false
         
         return tableView
@@ -38,10 +40,12 @@ final class ListViewController: UIViewController {
         
         configureNavigationBar()
         configureViewHierarchy()
+        configureTableView()
+        configureDataSource()
     }
     
     private func configureNavigationBar() {
-        navigationItem.title = "목록"
+        navigationItem.title = Constant.navigationTitle
     }
     
     private func configureViewHierarchy() {
@@ -54,5 +58,36 @@ final class ListViewController: UIViewController {
             listTableView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
             listTableView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor)
         ])
+    }
+    
+    private func configureTableView() {
+        listTableView.delegate = self
+    }
+    
+    private func configureDataSource() {
+        listDataSource = ListDataSource(tableView: listTableView) { tableView, indexPath, event in
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: Constant.ListCellIdentifier,
+                                                           for: indexPath) as? ListCell else {
+                return ListCell()
+            }
+            let startDate: String = DateFormatter.shared.string(from: event.startDate)
+            let endDate: String = DateFormatter.shared.string(from: event.endDate)
+            let dateText = startDate == endDate ? startDate : "\(startDate) ~ \(endDate)"
+            
+            cell.setText(title: event.title, date: dateText)
+            
+            return cell
+        }
+    }
+}
+
+extension ListViewController: UITableViewDelegate {}
+
+extension ListViewController {
+    
+    enum Constant {
+        
+        static let navigationTitle: String = "목록"
+        static let ListCellIdentifier: String = "ListCell"
     }
 }
