d296e844d5119b7a3ce79f09f3fc9cd33f0204d0
Gundy <korea1880@naver.com>
2023-09-30 10:15:10 +0900

    feat: 외부의 데이터 타입을 도메인의 event로 바꾸고 useCase에 적용하는 DataAdapter 프로토콜 정의

---
+++ FreeSeoulCulturalLife/FreeSeoulCulturalLife/InterfaceAdapters/DataAdapter/DataAdapter.swift
@@ -0,0 +1,33 @@
+//
+//  DataAdapter.swift
+//  FreeSeoulCulturalLife
+//
+//  Created by Gundy on 2023/09/30.
+//
+
+protocol DataAdapter {
+    
+    associatedtype DataType
+    
+    var useCase: UseCase { get }
+    
+    func convertToDomain(_ data: DataType) -> Event
+    func setDatas(_ datas: [DataType])
+    func addDatas(_ datas: [DataType])
+}
+
+extension DataAdapter {
+    
+    func setDatas(_ datas: [DataType]) {
+        let domainDatas = datas.map(convertToDomain)
+        
+        useCase.setContainer(domainDatas)
+    }
+    
+    func addDatas(_ datas: [DataType]) {
+        
+        let domainDatas = datas.map(convertToDomain)
+        
+        useCase.appendEvents(domainDatas)
+    }
+}
